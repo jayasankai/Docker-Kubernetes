c90b3a45ebb9f68775f560585ddae36d2ddd0aa8
jayasanka <61721893+jayasanka-ucsc@users.noreply.github.com>
2023-12-15 08:49:40 +1300

    repo changed

---
+++ 12-kub-action/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!!!</h1>
+    <p>This is new!</p>
+    <p>Try sending a request to /error and see what happens</p>
+  `);
+});
+
+app.get('/health', (req, res) => {
+  res.send(`
+    <p>Test Health</p>
+  `);
+});
+
+app.get('/error', (req, res) => {
+  process.exit(1);
+});
+
+app.listen(8080);
